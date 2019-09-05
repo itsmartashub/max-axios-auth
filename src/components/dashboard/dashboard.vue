5f7cf78fe634a6d267e05671d3ef3911032b2534
unknown <coderawii@gmail.com>
2019-09-05 22:35:30 +0200

    Global url, iliti baseURL, defaults, headers, interceptors - pre destructuringa tj odvajanja axiosa za ponaosob rikvestove

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+	<p>Your email address: {{ email }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+	data() {
+		return {
+			email: '' // treba da setujemo ovaj mail sa podacima koje smo dobili od servera, dakle u then() bloku jer tu pristupamo podacima. BTW da se zna, ne mozemo van tog then bloka, recimo posle opnog catch-a da napisemo this.email = resp.email, jer logicno, pristup responsu imamo samo unutar then() bloka jer je to ASINHRONI KOD, taj kod unutar then bloka se pokrene negde tamo u buducnosti, a kod nakon axios.get se opali momentalno nakon sto je ovo gotovo, bez da se ceka na taj future response
+		}
+	},
+
+	created() { // zelimo da ucitamo nesto kada se UCITA ova stranica/komponenta
+		axios.get('/korisnici.json') // za get() nam treba minimalno jedan argument, to je URL sa kojeg zelimo da dohvatimo nesto
+				.then(resp => {
+					console.log(resp)
+					// nas resp sadrzi js objekat u kom imamo pristup propertiju data, koji sadrzi unikatne cryptic keys oznake, kao neki id, gde su onda podaci o korisniku vrednost ovih kljuceva. Tako da ono sto bi trebalo da uradimo jeste da lupujemo kroz sve ove keys da dodjemo do email adresa 
+					// prvo hajde da objekat pretvorimo u niz da mzoemo da lupujemo kroz njega:
+					const podaci = resp.data
+					const arrKorisnici = [] // niz u koji cu sacuvati ovaj objekat tj prorpertije or whatever
+					for (let key in podaci) { // ovo key ce biti value of the cryptic keys
+						// arrKorisnici.push(podaci[key])
+						const korisnik = podaci[key] // da svaki taj key oznacava nekog korisnika
+						korisnik.id = key // dodelili smo svakom korisniku onaj cryptic key da bude id
+						arrKorisnici.push(korisnik)
+					}
+
+					console.log(arrKorisnici)
+					this.email = arrKorisnici[0].email
+				})
+				.catch(err => console.log(err))
+
+				// this.email = resp.email // NE VALJA OVDE POGLEDAJ GORE KOMENTAR ZASTO
+	}
+}
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios' // ovde smo importovali axios da bi mogli da setujemo kao neki global url, tj url nase baze koja se ne menja jelte, i na vise mesta u projektu se koristi, pa da se tamo ne ponavljamo, mozemo ovde da setujemo kao neki global, kao sto je recimo sa vue-resources islo sa http.options.root
+
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL = 'https://max-axios-auth.firebaseio.com' // base url, setovanje base url-a mozemo sa defaults objektom. Ovo je objekat sa kojim axios radi, i on nam omogucava da setujemo neka difoltna podesavanja i ta podesavanja koja setujemo ovde, mogu takodje biti setovana i tamo za recimo neki odredjen rikvest kao drugi/treci argument, ako zelimo da setujemo neko specificno podesavanje
+	// a ovde setujemo za global sto znaci da ce biti setovano za svaki rikvest. A ono sto zelimo za svaki rikvest je da se pristupoa istoj url adresi
+
+axios.defaults.headers.common['Authorization'] = 'neko setovanje za authorzation' // takodje mozemo da konfigurisemo headers. Elem, axios ce uvek staviti bar neki difoltni header, ali ako mi zelimo neki drugi, mozemo ovde da setujemo. Potom u headers imamonekoliko razlicitih targets: mozemo staviti common da setujemo headers sto ce se odraziti na svaki rikvest, bez obzira kog je tipa, potom mozemo da stavim onas licni header daodavajuci property u ['nas-licni-header'], recimo Authorization header i onda da ga setujemo u nesto, ovo je dobar nacin da se recimo posalje neki genericki token na backend za svaki rikvest
+
+axios.defaults.headers.get['Accepts'] = 'application/json' // ako zelimo da setujem oehgader za specijalni rikvest to mozemo ovako 
+// i sada mozemo da vidimo ovo u Network delu u web developer toolsu
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+	console.log('Request interceptor: ', config)
+	// config.headers.common['Authorization'] = 'neko setovanje za authorzation' // recimo, al mozemo staviti cime god zelimo da manipulisemo rikvest konfiguracijom. Recimo da proverimo da li je nesto slucaj, i onda usput poresimo konfiguraciju, za slucajeve gde bi neki genericki setovi kao recimo ovi gore (axios.defaults...) bili too much, too generic, gde god je neki rikvest targetivan
+	return config
+}) // interceptors (presretaci, prekidaci) su funkcije u kojima mozemo da definisemo koji podatak treba da executujemo za svaki rikvest koji napusta app??? ili koji god response da je postignut. Kada pristupimo interceptors objektu onda mozemo da biramo izmedju rikvesta i repsonsa u zavisnosti koji interceptors zelimo da kreiramo, i onda dodajemo novi interceptor koristeci use() metod. On sada prihvata f-ju, ta f-ja prima rikvest konfiguraciju kao argument, a unutar te f-je mi bar moramo da returnisemo ovu konfiguraciju, u suprotnom cemo BLOKIRATI RIKVEST
+
+const resInterceptor = axios.interceptors.response.use(res => {
+	console.log('Response interceptor: ', res)
+	return res //! i dakle obavezno moram da vratim res jer, u suprotnom nas kod u aplikaciji koji ceka res nikad ga nece primiti, dakle interceptors su uvek kao neki middleware (posrednik), radi izmedju, jer oni retko treba da blokiraju neki rispons ili rikvest
+})
+
+// ponekad nam treba to da uklonimo interceptor:
+axios.interceptors.request.eject(reqInterceptor) // sa eject() brisemo neki interceptor, konkretno ovde request interceptor, eject()-u treba ID interceptora, a ID je cesto vracen iz baze iz use()metoda, samo treba da ga sacuvamo u neke const
+axios.interceptors.response.eject(resInterceptor)
 
 Vue.config.productionTip = false
 
@@ -10,3 +35,6 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+//! stgo se tice axiosa, on je third party paket, i on se ne integrise sa vue kao ostali (vue) paketi sa Vue.use(imeImportaPaketa) vec ono, tamo gde zelimo da naopravimo rikvest, tamo importujemo axios from axios i pravimo rikvestove. Pa tako recimo zelimo da sabmitujemo podatke iz forme u signup.vue page i posle zelimo da mozemo da dohvatimo i potom pristupimo tim podacima sa dashboard.vue stranice
